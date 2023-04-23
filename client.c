a1decd1df6cc8d67f6228d7da4f8dccac1f2f8dd
Emanuel Oliveira <emanueloliveira38@yahoo.com.br>
2023-04-23 19:25:57 -0300

    v1

---
+++ client.c
@@ -0,0 +1,133 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <winsock2.h>
+#include <WS2tcpip.h>
+
+// to compile: gcc client.c -o client -lws2_32
+// to execute: ./client.exe
+
+int sendToServer(int sock, int op, char *data)
+{
+  if (data[strlen(data) - 1] == '\n')
+  {
+    data[strlen(data) - 1] = '\0';
+  }
+  char buffer[1024], res[1024];
+
+  itoa(op, buffer, 10);
+  strcat(buffer, ";");
+  strcat(buffer, data);
+  printf("\nMensagem enviada ao servidor: %s\n", buffer);
+  send(sock, buffer, strlen(buffer), 0);
+
+  recv(sock, res, sizeof(res), 0);
+  printf("\nMensagem recebida do servidor: %s\n", res);
+
+  return 0;
+}
+
+int options(int sock, int op)
+{
+  char data[1024];
+  if (op == 1)
+  {
+    printf("\nDigite os dados seguindo o modelo:\n");
+    printf("email;nome;sobrenome;cidade;curso-de-graduacao;ano-de-formacao;habilidade1,habilidade2,habilidade3\n");
+    printf("\nDados: ");
+    fgets(data, 1020, stdin);
+  }
+  else if (op == 2)
+  {
+    printf("\nDigite o curso:\n");
+    printf("\nCurso: ");
+    fgets(data, 1020, stdin);
+  }
+  else if (op == 3)
+  {
+    printf("\nDigite a habilidade:\n");
+    printf("\nHabilidade: ");
+    fgets(data, 1020, stdin);
+  }
+  else if (op == 4)
+  {
+    printf("\nDigite o ano de conslusao:\n");
+    printf("\nAno: ");
+    fgets(data, 1020, stdin);
+  }
+  else if (op == 5)
+  {
+    printf("\n");
+  }
+  else if (op == 6)
+  {
+    printf("\nDigite o email do perfil que deseja ver:\n");
+    printf("\nEmail: ");
+    fgets(data, 1020, stdin);
+  }
+  else if (op == 7)
+  {
+    printf("\nDigite o email do pefil que deseja excluir:\n");
+    printf("\nEmail: ");
+    fgets(data, 1020, stdin);
+  }
+  else if (op == 8)
+  {
+    printf("\nEncerrando...\n");
+  }
+  else
+  {
+    printf("\nOpcao invalida\n");
+    return 0;
+  }
+  sendToServer(sock, op, data);
+  return 0;
+}
+
+int main()
+{
+  char *ip = "127.0.0.1";
+  int port = 8080, sock, option = 0;
+  struct sockaddr_in addr;
+  socklen_t addr_size;
+
+  memset(&addr, '\0', sizeof(addr));
+  addr.sin_family = AF_INET;
+  addr.sin_port = port;
+  addr.sin_addr.s_addr = inet_addr(ip);
+
+  WSADATA Data;
+  WSAStartup(MAKEWORD(2, 2), &Data);
+  sock = socket(AF_INET, SOCK_STREAM, 0);
+  if (sock < 0)
+  {
+    perror("ERRO AO CONECTAR NO SOCKET");
+    exit(1);
+  }
+
+  connect(sock, (struct sockaddr *)&addr, sizeof(addr));
+  printf("##### SESSAO INICIADA #####\n");
+
+  while (option != 8)
+  {
+    printf("\nEscolha uma das opcoes:\n");
+    printf("1- Novo perfil;\n");
+    printf("2- Listar alunos por curso;\n");
+    printf("3- Listar alunos por habilidade;\n");
+    printf("4- Listar alunos por ano de conclusao;\n");
+    printf("5- Listar todos alunos;\n");
+    printf("6- Ver perfil;\n");
+    printf("7- Remover perfil;\n");
+    printf("8- Encerrar.\n\n");
+    printf("Opcao: ");
+    scanf("%d", &option);
+    fflush(stdin);
+    options(sock, option);
+  }
+
+  close(sock);
+  printf("\n##### SESSAO ENCERRADA #####\n");
+
+  return 0;
+}
---
+++ server.c
@@ -0,0 +1,463 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <winsock2.h>
+#include <WS2tcpip.h>
+#include <sys/types.h>
+#include <dirent.h>
+
+#define EMAIL 0
+#define NAME 1
+#define LASTNAME 2
+#define CITY 3
+#define COURSE 4
+#define YEAR 5
+#define SKILLS 6
+
+// to compile: gcc server.c -o server -lws2_32
+// to execute: ./server.exe
+
+// Profile's sample:
+// Email: maria_souza@gmail.com
+// Name: Maria
+// Last name: Souza
+// Residence: Campinas
+// Graduation: Ciência da Computação
+// Graduation year: 2018
+// Skills: Ciência dos Dados, Internet das Coisas, Computação em Nuvem
+
+// Register a new profile using email as identifier
+int newProfile(int client_sock, char data[], char res[])
+{
+  memset(res, '\0', 1024);
+  int i = 0;
+  char *p = strtok(data, ";"), *array[8];
+
+  while (p != NULL)
+  {
+    array[i++] = p;
+    p = strtok(NULL, ";");
+  }
+
+  char filename[104];
+  strcpy(filename, array[1]);
+  strcat(filename, ".txt");
+  FILE *file;
+  file = fopen(filename, "w");
+  if (file == NULL)
+  {
+    strcpy(res, "Perfil salvo com sucesso!");
+    return 0;
+  }
+  fprintf(file, array[1]);
+  fprintf(file, "\n");
+  fprintf(file, array[2]);
+  fprintf(file, "\n");
+  fprintf(file, array[3]);
+  fprintf(file, "\n");
+  fprintf(file, array[4]);
+  fprintf(file, "\n");
+  fprintf(file, array[5]);
+  fprintf(file, "\n");
+  fprintf(file, array[6]);
+  fprintf(file, "\n");
+  fprintf(file, array[7]);
+  fclose(file);
+  strcpy(res, "Perfil salvo com sucesso!");
+  return 0;
+}
+
+// List all profiles (email and name) graduated in a given course
+int listByCourse(char data[], char res[])
+{
+  memset(res, '\0', 1024);
+  int i = 0;
+  char *p = strtok(data, ";"), *array[2];
+  while (p != NULL)
+  {
+    array[i++] = p;
+    p = strtok(NULL, ";");
+  }
+  DIR *directory;
+  struct dirent *dir;
+  directory = opendir(".");
+  if (directory)
+  {
+    while ((dir = readdir(directory)) != NULL)
+    {
+      if (strstr(dir->d_name, ".txt") != NULL)
+      {
+        FILE *file;
+        char content[1024];
+        char profile[1024];
+        file = fopen(dir->d_name, "r");
+        for (i = 0; i <= COURSE; i++)
+        {
+          fgets(content, 100, file);
+          if (i == EMAIL)
+          {
+            strcpy(profile, content);
+            if (profile[strlen(profile) - 1] == '\n')
+            {
+              profile[strlen(profile) - 1] = '\0';
+            }
+            strcat(profile, ";");
+          }
+          else if (i == NAME)
+          {
+            strcat(profile, content);
+            if (profile[strlen(profile) - 1] == '\n')
+            {
+              profile[strlen(profile) - 1] = '\0';
+            }
+          }
+          else if (i == COURSE)
+          {
+            if (content[strlen(content) - 1] == '\n')
+            {
+              content[strlen(content) - 1] = '\0';
+            }
+          }
+        }
+        if (!strcmp(content, array[1]))
+        {
+          strcat(profile, "\n");
+          strcat(res, profile);
+          memset(profile, '\0', 1024);
+        }
+        fclose(file);
+      }
+    }
+    closedir(directory);
+  }
+  return 0;
+}
+
+// List all profiles (email and name) who have a certain skill
+int listBySkill(char data[], char res[])
+{
+  memset(res, '\0', 1024);
+  int i = 0;
+  char *p = strtok(data, ";"), *array[2];
+  while (p != NULL)
+  {
+    array[i++] = p;
+    p = strtok(NULL, ";");
+  }
+  if (array[1][strlen(array[1]) - 1] == '\n')
+  {
+    array[1][strlen(array[1]) - 1] = '\0';
+  }
+  DIR *directory;
+  struct dirent *dir;
+  directory = opendir(".");
+  if (directory)
+  {
+    while ((dir = readdir(directory)) != NULL)
+    {
+      if (strstr(dir->d_name, ".txt") != NULL)
+      {
+        FILE *file;
+        char content[1024];
+        char profile[1024];
+        file = fopen(dir->d_name, "r");
+        for (i = 0; i <= SKILLS; i++)
+        {
+          fgets(content, 100, file);
+          if (i == EMAIL)
+          {
+            strcpy(profile, content);
+            if (profile[strlen(profile) - 1] == '\n')
+            {
+              profile[strlen(profile) - 1] = '\0';
+            }
+            strcat(profile, ";");
+          }
+          else if (i == NAME)
+          {
+            strcat(profile, content);
+            if (profile[strlen(profile) - 1] == '\n')
+            {
+              profile[strlen(profile) - 1] = '\0';
+            }
+          }
+        }
+        if (strstr(content, array[1]))
+        {
+          strcat(profile, "\n");
+          strcat(res, profile);
+          memset(profile, '\0', 1024);
+        }
+        fclose(file);
+      }
+    }
+    closedir(directory);
+  }
+  return 0;
+}
+
+// List all profiles (email, name and course) graduated in a given year
+int listByConclusionYear(char data[], char res[])
+{
+  memset(res, '\0', 1024);
+  int i = 0;
+  char *p = strtok(data, ";"), *array[2];
+  while (p != NULL)
+  {
+    array[i++] = p;
+    p = strtok(NULL, ";");
+  }
+  DIR *directory;
+  struct dirent *dir;
+  directory = opendir(".");
+  if (directory)
+  {
+    while ((dir = readdir(directory)) != NULL)
+    {
+      if (strstr(dir->d_name, ".txt") != NULL)
+      {
+        FILE *file;
+        char content[1024];
+        char profile[1024];
+        file = fopen(dir->d_name, "r");
+        for (i = 0; i <= YEAR; i++)
+        {
+          fgets(content, 100, file);
+          if (i == EMAIL)
+          {
+            strcpy(profile, content);
+            if (profile[strlen(profile) - 1] == '\n')
+            {
+              profile[strlen(profile) - 1] = '\0';
+            }
+            strcat(profile, ";");
+          }
+          else if (i == NAME)
+          {
+            strcat(profile, content);
+            if (profile[strlen(profile) - 1] == '\n')
+            {
+              profile[strlen(profile) - 1] = '\0';
+            }
+            strcat(profile, ";");
+          }
+          else if (i == COURSE)
+          {
+            strcat(profile, content);
+            if (profile[strlen(profile) - 1] == '\n')
+            {
+              profile[strlen(profile) - 1] = '\0';
+            }
+          }
+          else if (i == YEAR)
+          {
+            if (content[strlen(content) - 1] == '\n')
+            {
+              content[strlen(content) - 1] = '\0';
+            }
+          }
+        }
+        if (!strcmp(content, array[1]))
+        {
+          strcat(profile, "\n");
+          strcat(res, profile);
+          memset(profile, '\0', 1024);
+        }
+        fclose(file);
+      }
+    }
+    closedir(directory);
+  }
+  return 0;
+}
+
+// List all information from all profiles
+int listAll(char res[])
+{
+  memset(res, '\0', 1024);
+  int i = 0;
+  DIR *directory;
+  struct dirent *dir;
+  directory = opendir(".");
+  if (directory)
+  {
+    while ((dir = readdir(directory)) != NULL)
+    {
+      if (strstr(dir->d_name, ".txt") != NULL)
+      {
+        FILE *file;
+        char content[1024];
+        char profile[1024];
+        file = fopen(dir->d_name, "r");
+        for (i = 0; i <= SKILLS; i++)
+        {
+          fgets(content, 100, file);
+          strcat(profile, content);
+          if (profile[strlen(profile) - 1] == '\n')
+          {
+            profile[strlen(profile) - 1] = '\0';
+          }
+          if (i != SKILLS)
+          {
+            strcat(profile, ";");
+          }
+        }
+        strcat(profile, "\n");
+        strcat(res, profile);
+        memset(profile, '\0', 1024);
+        fclose(file);
+      }
+    }
+    closedir(directory);
+  }
+  return 0;
+}
+
+// Given a profile's email, return their information
+int getProfile(char data[], char res[])
+{
+  memset(res, '\0', 1024);
+  int i = 0;
+  char *p = strtok(data, ";"), *array[2];
+
+  while (p != NULL)
+  {
+    array[i++] = p;
+    p = strtok(NULL, ";");
+  }
+  if (array[1][strlen(array[1]) - 1] == '\n')
+  {
+    array[1][strlen(array[1]) - 1] = '\0';
+  }
+  char content[1024];
+  char profile[1024];
+  char filename[104];
+  FILE *file;
+  strcpy(filename, array[1]);
+  strcat(filename, ".txt");
+  file = fopen(filename, "r");
+  for (i = 0; i <= SKILLS; i++)
+  {
+    fgets(content, 100, file);
+    if (content[strlen(content) - 1] == '\n')
+    {
+      content[strlen(content) - 1] = '\0';
+    }
+    strcat(profile, content);
+    if (i != SKILLS)
+    {
+      strcat(profile, ";");
+    }
+  }
+  strcat(res, profile);
+  memset(profile, '\0', 1024);
+  fclose(file);
+  return 0;
+}
+
+// Removing a profile from its identifier (email)
+int removeProfile(char data[], char res[])
+{
+  memset(res, '\0', 1024);
+  int i = 0;
+  char *p = strtok(data, ";"), *array[2];
+
+  while (p != NULL)
+  {
+    array[i++] = p;
+    p = strtok(NULL, ";");
+  }
+  char filename[104];
+  strcpy(filename, array[1]);
+  strcat(filename, ".txt");
+  int delete = remove(filename);
+  if (delete == 0)
+  {
+    strcpy(res, "Perfil apagado com sucesso!");
+    return 0;
+  }
+  strcpy(res, "Perfil não encontrado!");
+  return 0;
+}
+
+int main()
+{
+  char *ip = "127.0.0.1", buffer[1024], res[1024];
+  int port = 8080, server_sock, client_sock, n;
+  struct sockaddr_in server_addr, client_addr;
+  socklen_t addr_size;
+  pid_t pid;
+
+  WSADATA Data;
+  WSAStartup(MAKEWORD(2, 2), &Data);
+  server_sock = socket(AF_INET, SOCK_STREAM, 0);
+  if (server_sock < 0)
+  {
+    perror("ERRO AO CRIAR SERVER SOCKET");
+    exit(1);
+  }
+  printf("##### SESSAO INICIADA #####\n");
+
+  memset(&server_addr, '\0', sizeof(server_addr));
+  server_addr.sin_family = AF_INET;
+  server_addr.sin_port = port;
+  server_addr.sin_addr.s_addr = inet_addr(ip);
+
+  n = bind(server_sock, (struct sockaddr *)&server_addr, sizeof(server_addr));
+  if (n < 0)
+  {
+    perror("ERRO AO FAZER BIND DE SOCKET ID A ESTRUTURA DE SOCKET");
+    exit(1);
+  }
+  listen(server_sock, 5);
+
+  addr_size = sizeof(client_addr);
+  client_sock = accept(server_sock, (struct sockaddr *)&client_addr, &addr_size);
+  while (1)
+  {
+    recv(client_sock, buffer, sizeof(buffer), 0);
+    printf("\nMensagem recebida do cliente %d: %s\n", client_sock, buffer);
+    if (strlen(buffer) != 0)
+    {
+      switch (buffer[0])
+      {
+      case '1':
+        newProfile(client_sock, buffer, res);
+        break;
+      case '2':
+        listByCourse(buffer, res);
+        break;
+      case '3':
+        listBySkill(buffer, res);
+        break;
+      case '4':
+        listByConclusionYear(buffer, res);
+        break;
+      case '5':
+        listAll(res);
+        break;
+      case '6':
+        getProfile(buffer, res);
+        break;
+      case '7':
+        removeProfile(buffer, res);
+        break;
+      case '8':
+        close(client_sock);
+        exit(0);
+      default:
+        break;
+      }
+      send(client_sock, res, strlen(res), 0);
+      printf("\nMensagem enviada ao cliente %d: %s\n", client_sock, res);
+      memset(buffer, 0, 1024);
+      memset(res, 0, 1024);
+    }
+  }
+  close(server_sock);
+
+  printf("\n##### SESSAO ENCERRADA #####\n");
+
+  return 0;
+}
